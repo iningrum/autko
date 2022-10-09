48f09d5873c44042696e7a6fe3fc49d2ddc7ed34
iningrum <dorin_ingrum@outlook.com>
2022-10-09 13:24:17 -0700

    Created static library CarManager
    namespace Lights

--- main.cpp
+++ main.cpp
@@ -4,18 +4,16 @@
  * Created: 10/9/2022 10:34:14 AM
  * Author : WIN-11K
  */ 
-#define  F_CPU 1600000UL
-#include <avr/io.h>
-#include <util/delay.h>
-
+#include "main.h"
+#include <CarManager/lights.h>
 int main(void)
 {
-	DDRB |= 0xCC;
-	PORTB |= 0xCC;
+	Lights::Setup();
+	Lights::On(true);
     /* Replace with your application code */
     while (1) 
     {
 		_delay_ms(10000);
-		PORTB ^= 0xCC;
+		Lights::Toggle(true);
     }
 }
---
+++ main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ * Created: 10/9/2022 12:12:16 PM
+ *  Author: WIN-11K
+ */ 
+
+
+#ifndef MAIN_H_
+#define MAIN_H_
+#define  F_CPU 1600000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+#endif /* MAIN_H_ */
